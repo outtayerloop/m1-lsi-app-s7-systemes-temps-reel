8d14707b77d9972922a4c8edca5e9f7c25e8a673
Wiem Chouchane <wiem.chouchane@efrei.net>
2020-10-30 13:41:52 +0100

    Ex 2 q3

--- TP2/exo2_q3.c
+++ TP2/exo2_q3.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
 
 #define SIGUSR1_NUM 10
 #define CONSTANT_PROC 220000
 
 void do_work();
-
-void fork_parent();
 
 int main(int argc, char** argv){
 
@@ -17,39 +13,19 @@ int main(int argc, char** argv){
     printf("Mon PID est : %d\n", parent_pid);
 
     //Handler pour le signal SIGUSR1
-    signal(SIGUSR1_NUM, fork_parent);
+    signal(SIGUSR1_NUM, do_work);
 
     //Mise en attente du signal
     int signal_received = pause();
 
     //Reception du signal
     if(signal_received == -1){
-
-        int current_pid = getpid();
-
-        if(current_pid == -1){
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        // Fils
-        if(current_pid != parent_pid){
-            do_work();
-            exit(EXIT_SUCCESS);
-        }
-        // Pere
-        else{
-            while(wait(NULL) != -1);
-            perror("pause");
-            exit(EXIT_SUCCESS);
-        }
+        perror("pause");
+        return -1;
     }
 
     return 0;
 }
-
-void fork_parent(){
-    fork();
-};
 
 void do_work()
 {
