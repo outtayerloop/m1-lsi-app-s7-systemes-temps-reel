8466781c354f887964fd454570e9828775a49cdf
Wiem Chouchane <wiem.chouchane@efrei.net>
2020-10-30 14:13:03 +0100

    Ex2 q4 : Infinite loop for multiple same signal handling

---
+++ TP2/exo2_q4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define SIGUSR1_NUM 10
+#define CONSTANT_PROC 220000
+
+void do_work();
+
+int main(int argc, char** argv){
+
+    int pid = getpid();
+    printf("Mon PID est : %d\n", pid);
+
+    //Handler pour le signal SIGUSR1
+    signal(SIGUSR1_NUM, do_work);
+
+    //Mise en attente du signal
+    int signal_received = pause();
+
+    while(1){
+        sleep(1);
+        printf("alive.\n");
+    }
+
+    return 0;
+}
+
+void do_work()
+{
+    unsigned int nb_millisecondes = 2000;
+    unsigned int i = CONSTANT_PROC * nb_millisecondes; //attention a ne pas utiliser un int pour eviter un overflow
+
+    while(i>0)
+    {
+        asm volatile("nop");
+        i--;
+    }
+
+    return;
+}
